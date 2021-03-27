63fbfd722c765eb9cede8c9b6b6fe9fb6aa5d668
Ezequiel <74028574+Ezequiel-GEO@users.noreply.github.com>
2021-03-27 12:24:59 -0300

    Add files via upload

---
+++ SMR_GUI.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("SMR (Romana, 1989)")
+root.geometry("800x500")
+
+intro = Label(root,text="Calcular el RMRb y luego indicar la orientación de la familia de discontinuidad y talud. Calcular el SMR para cada familia y adoptar el menor valor.")
+intro.place(x=10, y=10)
+
+
+
+
+root.mainloop()
