ba4b5bc6e54a85a29525ff133896636e80007d9f
Ezequiel <74028574+Ezequiel-GEO@users.noreply.github.com>
2021-04-08 17:14:18 -0300

    Update Plot stereo.py

--- Plot stereo.py
+++ Plot stereo.py
@@ -1,11 +1,26 @@
 import pandas
 import numpy
 import tkinter
+import mplstereonet
 from tkinter import *
-import matplotlib
+import matplotlib.pyplot as plt
 
-root = Tk()
-root.title ("Proyección estereográfica")
 
-root.mainloop()
+
+strike1, dip1 = 10, 30
+strike2, dip2 = 315, 78
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='stereonet')
+ax.plane(strike1, dip1, c='b', label='Bedding %03d/%02d' % (strike1, dip1))
+ax.plane(strike2, dip2, c='r', label='Fault %03d/%02d' % (strike2, dip2))
+ax.legend()
+plunge, bearing = mplstereonet.plane_intersection(strike1, dip1, strike2, dip2)
+ax.line(plunge, bearing, 'ko', markersize=5, label='Intersection %02d/%03d' % (plunge, bearing))
+ax.legend()
+# We can also add a grid
+ax.grid()
+fig
+
+plt.show()
 
